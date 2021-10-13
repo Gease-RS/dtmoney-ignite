653a229f257789bc4657129284eee131c7efee79
Gease-RS <geasebrasil@gmail.com>
2021-10-12 23:02:10 -0300

    Modal & Forms concluído

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from '../../services/api'
 import { Container } from "./styles";
 
+interface Transactions {
+    id: number;
+    title: string;
+    type: string;
+    category: string;
+    amount: number;
+    createdAt: string;
+}
+
 export function TransactionsTable() {
+    const [transactions, setTransactions] = useState<Transactions[]>([])
+
     useEffect(() => {
-        api.get('transactions')
-            .then(response => console.log(response.data))
+        api.get<{transactions: Transactions[]}>('/transactions')
+            .then(response => setTransactions(response.data.transactions));
     }, [])
 
     return (
@@ -21,31 +32,23 @@ export function TransactionsTable() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td className="title">Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td className="title">Aluguel</td>
-                        <td className="withdraw">- R$ 1.100</td>
-                        <td>Casa</td>
-                        <td>17/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td className="title">Desenvolvimento de website</td>
-                        <td>R$ 12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td className="title">Desenvolvimento de website</td>
-                        <td>R$ 12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-
+                    {transactions.map(transaction =>  (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>
+                                {new Intl.DateTimeFormat('pt-BR').format(
+                                    new Date(transaction.createdAt)
+                                )}
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
